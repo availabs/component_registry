bca009332ccc6e7b4088d2a09c0fad0626f6353e
shaunaksangdod <shaunaksangdod@gmail.com>
2024-02-05 10:18:35 -0500

    disaster loss stats comp

--- DisasterLossStats/components/RenderDisasterLossStats.jsx
+++ DisasterLossStats/components/RenderDisasterLossStats.jsx
@@ -7,58 +7,114 @@ const blockClass = `w-full h-[90px] bg-white p-1 text-center flex flex-col`,
     blockLabelClass = `border-b-2`,
     blockValueClass = `font-medium text-xl pt-2`
 
+const RenderLoss = ({totalLoss, ihpLoss, paLoss, sbaLoss, nfipLoss, usdaLoss}) => (
+    <>
+        <div className={blockClass}>
+            <label className={blockLabelClass}>Total Loss</label>
+            <span className={blockValueClass}>{
+                fnumIndex(totalLoss, 2, true)
+            }</span>
+        </div>
+        <div className={blockClass}>
+            <label className={blockLabelClass}>IHP Loss</label>
+            <span className={blockValueClass}>{
+                fnumIndex(ihpLoss, 2, true)
+            }</span>
+        </div>
+        <div className={blockClass}>
+            <label className={blockLabelClass}>PA Loss</label>
+            <span className={blockValueClass}>{
+                fnumIndex(paLoss, 2, true)
+            }</span>
+        </div>
+        <div className={blockClass}>
+            <label className={blockLabelClass}>SBA Loss</label>
+            <span className={blockValueClass}>{
+                fnumIndex(sbaLoss, 2, true)
+            }</span>
+        </div>
+        <div className={blockClass}>
+            <label className={blockLabelClass}>NFIP Loss</label>
+            <span className={blockValueClass}>{
+                fnumIndex(nfipLoss, 2, true)
+            }</span>
+        </div>
+        <div className={blockClass}>
+            <label className={blockLabelClass}>USDA Loss</label>
+            <span className={blockValueClass}>{
+                fnumIndex(usdaLoss, 2, true)
+            }</span>
+        </div>
+    </>
+)
+
+const RenderFunding = ({totalFunding, ihpFunding, paFunding, sbaFunding, hmgpFunding}) => (
+    <>
+        <div className={blockClass}>
+            <label className={blockLabelClass}>Total Funding</label>
+            <span className={blockValueClass}>{
+                fnumIndex(totalFunding, 2, true)
+            }</span>
+        </div>
+        <div className={blockClass}>
+            <label className={blockLabelClass}>IHP Funding</label>
+            <span className={blockValueClass}>{
+                fnumIndex(ihpFunding, 2, true)
+            }</span>
+        </div>
+        <div className={blockClass}>
+            <label className={blockLabelClass}>PA Funding</label>
+            <span className={blockValueClass}>{
+                fnumIndex(paFunding, 2, true)
+            }</span>
+        </div>
+        <div className={blockClass}>
+            <label className={blockLabelClass}>SBA Funding</label>
+            <span className={blockValueClass}>{
+                fnumIndex(sbaFunding, 2, true)
+            }</span>
+        </div>
+        <div className={blockClass}>
+            <label className={blockLabelClass}>HMGP Funding</label>
+            <span className={blockValueClass}>{
+                fnumIndex(hmgpFunding, 2, true)
+            }</span>
+        </div>
+    </>
+)
 export const RenderDisasterLossStats = ({
-                                            totalLoss, ihpLoss, paLoss, sbaLoss,
-                                            nfipLoss, usdaLoss, hmgpFunding,
+                                            type, 
+                                            totalLoss, ihpLoss, paLoss, sbaLoss, nfipLoss, usdaLoss,
+                                            totalFunding, ihpFunding, paFunding, sbaFunding, hmgpFunding,
                                             attributionData, baseUrl
-}) => {
-   return (
-       <React.Fragment>
-           <div className={"w-full grid grid-cols-7 gap-4 place-content-stretch mt-5"}>
-               <div className={blockClass}>
-                   <label className={blockLabelClass}>Total Loss</label>
-                   <span className={blockValueClass}>{
-                       fnumIndex(totalLoss, 2, true)
-                   }</span>
-               </div>
-               <div className={blockClass}>
-                   <label className={blockLabelClass}>IHP Loss</label>
-                   <span className={blockValueClass}>{
-                       fnumIndex(ihpLoss, 2, true)
-                   }</span>
-               </div>
-               <div className={blockClass}>
-                   <label className={blockLabelClass}>PA Loss</label>
-                   <span className={blockValueClass}>{
-                       fnumIndex(paLoss, 2, true)
-                   }</span>
-               </div>
-               <div className={blockClass}>
-                   <label className={blockLabelClass}>SBA Loss</label>
-                   <span className={blockValueClass}>{
-                       fnumIndex(sbaLoss, 2, true)
-                   }</span>
-               </div>
-               <div className={blockClass}>
-                   <label className={blockLabelClass}>NFIP Loss</label>
-                   <span className={blockValueClass}>{
-                       fnumIndex(nfipLoss, 2, true)
-                   }</span>
-               </div>
-               <div className={blockClass}>
-                   <label className={blockLabelClass}>USDA Loss</label>
-                   <span className={blockValueClass}>{
-                       fnumIndex(usdaLoss, 2, true)
-                   }</span>
-               </div>
-               <div className={blockClass}>
-                   <label className={`${blockLabelClass}`}>HMGP Funding</label>
-                   <span className={blockValueClass}>{
-                       fnumIndex(hmgpFunding, 2, true)
-                   }</span>
-               </div>
-           </div>
-           <Attribution baseUrl={baseUrl} attributionData={attributionData} />
-       </React.Fragment>
-   )
+                                        }) => {
+    const colsByType = {
+        'loss': 6,
+        'funding': 5
+    }
+    return (
+        <React.Fragment>
+            <div className={`w-full grid grid-cols-${colsByType[type]} gap-4 place-content-stretch mt-5`}>
+                {
+                    type === 'loss' ? 
+                        <RenderLoss 
+                            totalLoss={totalLoss} 
+                            ihpLoss={ihpLoss}
+                            paLoss={paLoss}
+                            sbaLoss={sbaLoss}
+                            nfipLoss={nfipLoss} 
+                            usdaLoss={usdaLoss} 
+                        /> :
+                        <RenderFunding
+                            totalFunding={totalFunding}
+                            ihpFunding={ihpFunding}
+                            paFunding={paFunding}
+                            sbaFunding={sbaFunding}
+                            hmgpFunding={hmgpFunding}
+                        />
+                }
+            </div>
+            <Attribution baseUrl={baseUrl} attributionData={attributionData}/>
+        </React.Fragment>
+    )
 };
--- DisasterLossStats/index.jsx
+++ DisasterLossStats/index.jsx
@@ -8,32 +8,33 @@ import VersionSelectorSearchable from "../shared/versionSelector/searchable.jsx"
 import GeographySearch from "../shared/geographySearch.jsx";
 import DisasterSearch from "../shared/disasterSearch.jsx";
 import { Loading } from "~/utils/loading.jsx";
+import {ButtonSelector} from "../shared/buttonSelector.jsx";
 
-const colAccessNameMapping = {
-    'disaster_number': 'distinct disaster_number as disaster_number',
-}
 
-async function getData ({ealViewId, geoid, disasterNumber}, falcor) {
+async function getData ({ealViewId, geoid, disasterNumber, type}, falcor) {
     const dependencyPath = (view_id) => ["dama", pgEnv, "viewDependencySubgraphs", "byViewId", view_id];
     const res = await falcor.get(dependencyPath(ealViewId))
     const deps = get(res, ["json", ...dependencyPath(ealViewId), "dependencies"]);
-    const dlsDeps = deps.find(dep => dep.type === "disaster_loss_summary");
 
- 
-    const disasterLossView = dlsDeps.view_id
-    const hmgpView = 798;
+    // views to get loss columns
+    const disasterLossView = deps.find(dep => dep.type === "disaster_loss_summary").view_id;
     const disasterWebSummariesView = 512;
 
+
+    // views to get funding columns
+    const ihpView = deps.find(dep => dep.type === "individuals_and_households_program_valid_registrations_v1").view_id;
+    const paView = deps.find(dep => dep.type === "public_assistance_funded_projects_details_v1_enhanced").view_id;
+    const sbaView = deps.find(dep => dep.type === "sba_disaster_loan_data_new").view_id;
+    const hmgpView = 798;
+
+    // config to get loss columns
     const geoIdCol = geoid?.toString()?.length === 2 ? "substring(geoid, 1, 2)" : geoid?.toString()?.length === 5 ? "geoid" : `'all'`,
         disasterDetailsAttributes = {
-    // ...disasterNumber && {disaster_number: "disaster_number"},
             geoid: `${geoIdCol} as geoid`,
-            fema_property_damage: "sum(fema_property_damage) as fema_property_damage",
             ihp_loss: "sum(ihp_loss) as ihp_loss",
             pa_loss: "sum(pa_loss) as pa_loss",
             sba_loss: "sum(sba_loss) as sba_loss",
             nfip_loss: "sum(nfip_loss) as nfip_loss",
-            // hmgp_funding: "sum(hmgp_funding) as hmgp_funding",
             fema_crop_damage: "sum(fema_crop_damage) as fema_crop_damage"
         },
         disasterDetailsOptions = JSON.stringify({
@@ -46,17 +47,6 @@ async function getData ({ealViewId, geoid, disasterNumber}, falcor) {
         }),
         disasterDetailsPath = (view_id) => ["dama", pgEnv, "viewsbyId", view_id, "options", disasterDetailsOptions];
 
-    const hmgpAttributes = {
-            hmgp_funding: "SUM(federal_share_obligated) as hmgp_funding",
-        },
-        hmgpOptions = JSON.stringify({
-            filter: {
-                ...disasterNumber && {disaster_number: [disasterNumber]}
-            },
-        }),
-       hmgpPath = ["dama", pgEnv, "viewsbyId", hmgpView, "options", hmgpOptions];
-
-
     const disasterWebSummariesAttributes = {
             ihp_loss: "total_amount_ihp_approved as ihp_loss",
             pa_loss: "total_obligated_amount_pa as pa_loss"
@@ -68,21 +58,91 @@ async function getData ({ealViewId, geoid, disasterNumber}, falcor) {
         }),
         disasterWebSummariesPath = (view_id) => ["dama", pgEnv, "viewsbyId", view_id, "options", disasterWebSummariesOptions];
 
-    const finalResp = await falcor.get(
-        [...disasterDetailsPath(dlsDeps.view_id), "databyIndex", { from: 0, to: 0 }, Object.values(disasterDetailsAttributes)],
-        [...hmgpPath, "databyIndex", { from: 0, to: 0 }, Object.values(hmgpAttributes)],
-        [...disasterWebSummariesPath(disasterWebSummariesView), "databyIndex", { from: 0, to: 0 }, Object.values(disasterWebSummariesAttributes)],
-        ['dama', pgEnv, 'views', 'byId', [dlsDeps.view_id, disasterWebSummariesView], 'attributes', ['source_id', 'view_id', 'version', '_modified_timestamp']]
-    );
+    // configs to get funding columns
+    const ihpGeoIdCOl = geoid?.toString()?.length === 2 ? "substring(geoid, 1, 2)" : geoid?.toString()?.length === 5 ? "geoid" : `'all'`,
+        ihpAttributes = {
+            ihp_funding: "SUM(ihp_amount) as ihp_amount",
+        },
+        ihpOptions = JSON.stringify({
+            filter: {
+                ...geoid && {[ihpGeoIdCOl]: [geoid]},
+                ...disasterNumber && {disaster_number: [disasterNumber]}
+            },
+        }),
+       ihpPath = ["dama", pgEnv, "viewsbyId", ihpView, "options", ihpOptions];
 
-    const ihpLoss = get(finalResp, ['json', ...disasterWebSummariesPath(disasterWebSummariesView), "databyIndex", 0, disasterWebSummariesAttributes.ihp_loss], 0),
+    const paGeoIdCOl = geoid?.toString()?.length === 2 ? "lpad(state_number_code::text, 2, '0')" :
+                                geoid?.toString()?.length === 5 ? "lpad(state_number_code::text, 2, '0') || lpad(county_code::text, 3, '0')" :
+                                    `'all'`,
+        paAttributes = {
+            pa_funding: "SUM(total_obligated) as total_obligated",
+        },
+        paOptions = JSON.stringify({
+            filter: {
+                ...geoid && {[paGeoIdCOl]: [geoid]},
+                ...disasterNumber && {disaster_number: [disasterNumber]}
+            },
+        }),
+       paPath = ["dama", pgEnv, "viewsbyId", paView, "options", paOptions];
+
+    const sbaGeoIdCOl = geoid?.toString()?.length === 2 ? "substring(geoid, 1, 2)" : geoid?.toString()?.length === 5 ? "geoid" : `'all'`,
+        sbaAttributes = {
+            sba_funding: "SUM(total_approved_loan_amount) as total_approved_loan_amount",
+        },
+        sbaOptions = JSON.stringify({
+            filter: {
+                ...geoid && {[sbaGeoIdCOl]: [geoid]},
+                ...disasterNumber && {fema_disaster_number: [disasterNumber]}
+            },
+        }),
+       sbaPath = ["dama", pgEnv, "viewsbyId", sbaView, "options", sbaOptions];
+
+    const hmgpGeoIdCOl = geoid?.toString()?.length === 2 ? "state_number_code" :
+            geoid?.toString()?.length === 5 ? "state_number_code || county_code" : `'all'`,
+        hmgpAttributes = {
+            hmgp_funding: "SUM(federal_share_obligated) as hmgp_funding",
+        },
+        hmgpOptions = JSON.stringify({
+            filter: {
+                ...geoid && {[hmgpGeoIdCOl]: [geoid]},
+                ...disasterNumber && {disaster_number: [disasterNumber]}
+            },
+        }),
+       hmgpPath = ["dama", pgEnv, "viewsbyId", hmgpView, "options", hmgpOptions];
+
+    const paths = type === 'loss' ?
+        [
+            [...disasterDetailsPath(disasterLossView), "databyIndex", { from: 0, to: 0 }, Object.values(disasterDetailsAttributes)],
+            [...disasterWebSummariesPath(disasterWebSummariesView), "databyIndex", { from: 0, to: 0 }, Object.values(disasterWebSummariesAttributes)],
+            ['dama', pgEnv, 'views', 'byId', [disasterLossView], 'attributes', ['source_id', 'view_id', 'version', '_modified_timestamp']]
+        ] :
+        [
+            [...ihpPath, "databyIndex", { from: 0, to: 0 }, Object.values(ihpAttributes)],
+            [...paPath, "databyIndex", { from: 0, to: 0 }, Object.values(paAttributes)],
+            [...sbaPath, "databyIndex", { from: 0, to: 0 }, Object.values(sbaAttributes)],
+            [...hmgpPath, "databyIndex", { from: 0, to: 0 }, Object.values(hmgpAttributes)],
+            ['dama', pgEnv, 'views', 'byId', [ihpView, paView, sbaView, hmgpView], 'attributes', ['source_id', 'view_id', 'version', '_modified_timestamp']]
+        ]
+
+    const finalResp = await falcor.get(...paths);
+
+    const
+        // ihpLoss = get(finalResp, ['json', ...disasterDetailsPath(disasterLossView), "databyIndex", 0, disasterDetailsAttributes.ihp_loss], 0),
+        // paLoss = get(finalResp, ['json', ...disasterDetailsPath(disasterLossView), "databyIndex", 0, disasterDetailsAttributes.pa_loss], 0),
+        // ihp and pa pulled from WDS slightly differ from ihp and pa tables.
+        ihpLoss = get(finalResp, ['json', ...disasterWebSummariesPath(disasterWebSummariesView), "databyIndex", 0, disasterWebSummariesAttributes.ihp_loss], 0),
         paLoss = get(finalResp, ['json', ...disasterWebSummariesPath(disasterWebSummariesView), "databyIndex", 0, disasterWebSummariesAttributes.pa_loss], 0),
         sbaLoss = get(finalResp, ['json', ...disasterDetailsPath(disasterLossView), "databyIndex", 0, disasterDetailsAttributes.sba_loss], 0),
         nfipLoss = get(finalResp, ['json', ...disasterDetailsPath(disasterLossView), "databyIndex", 0, disasterDetailsAttributes.nfip_loss], 0),
         usdaLoss = get(finalResp, ['json', ...disasterDetailsPath(disasterLossView), "databyIndex", 0, disasterDetailsAttributes.fema_crop_damage], 0),
+
+        ihpFunding = get(finalResp, ['json', ...ihpPath, "databyIndex", 0, ihpAttributes.ihp_funding], 0),
+        paFunding = get(finalResp, ['json', ...paPath, "databyIndex", 0, paAttributes.pa_funding], 0),
+        sbaFunding = get(finalResp, ['json', ...sbaPath, "databyIndex", 0, sbaAttributes.sba_funding], 0),
         hmgpFunding = get(finalResp, ['json', ...hmgpPath, "databyIndex", 0, hmgpAttributes.hmgp_funding], 0);
         
     return {
+        type,
         ealViewId,
         disasterNumber,
         geoid,
@@ -92,9 +152,15 @@ async function getData ({ealViewId, geoid, disasterNumber}, falcor) {
         sbaLoss,
         nfipLoss,
         usdaLoss,
+
+        ihpFunding,
+        paFunding,
+        sbaFunding,
         hmgpFunding,
-        totalLoss: (+ihpLoss + +paLoss + +sbaLoss + +nfipLoss + +usdaLoss + +hmgpFunding),
-        attributionData: [disasterLossView, disasterWebSummariesView]
+
+        totalLoss: (+ihpLoss + +paLoss + +sbaLoss + +nfipLoss + +usdaLoss),
+        totalFunding: (+ihpFunding + +paFunding + +sbaFunding + +hmgpFunding),
+        attributionData: (type === 'loss' ? [disasterLossView] : [ihpView, paView, sbaView, hmgpView])
             .map(view => get(finalResp, ['json','dama', pgEnv, 'views', 'byId', view, 'attributes'], {}))
     }
 }
@@ -113,6 +179,7 @@ const Edit = ({value, onChange}) => {
     const [loading, setLoading] = useState(true);
     const [status, setStatus] = useState(cachedData?.status);
     const [geoid, setGeoid] = useState(cachedData?.geoid === '' ? cachedData?.geoid : (cachedData?.geoid || '36'));
+    const [type, setType] = useState(cachedData?.type || 'loss');
 
     useEffect( () => {
         async function load(){
@@ -124,15 +191,15 @@ const Edit = ({value, onChange}) => {
             }
             setLoading(true);
             setStatus(undefined);
-            const data = await getData({geoid,disasterNumber,ealViewId},falcor)
-            if(data.totalLoss) {
+            const data = await getData({geoid,disasterNumber,ealViewId, type},falcor)
+            if(data) {
                 onChange(JSON.stringify(data))
             }
             setLoading(false);
         }
 
         load()
-    }, [geoid, ealViewId, disasterNumber]);
+    }, [geoid, ealViewId, disasterNumber, type]);
 
 
     return (
@@ -150,16 +217,28 @@ const Edit = ({value, onChange}) => {
                         showAll={true}
                         className={'flex-row-reverse'}
                     />
+                    <ButtonSelector
+                        type={type}
+                        setType={setType}
+                        label={'Type:'}
+                        types={[{value: 'loss', label: 'Assessed Loss'}, {value: 'funding', label: 'FEMA Obligated Funding'}]}
+                    />
                 </div>
                 {loading ? <Loading /> :
                     status ? <div className={'p-5 text-center'}>{status}</div> :
                     <RenderDisasterLossStats
+                        type={cachedData.type}
                         totalLoss={cachedData.totalLoss}
                         ihpLoss={cachedData.ihpLoss}
                         paLoss={cachedData.paLoss}
                         sbaLoss={cachedData.sbaLoss}
                         nfipLoss={cachedData.nfipLoss}
                         usdaLoss={cachedData.usdaLoss}
+
+                        totalFunding={cachedData.totalFunding}
+                        ihpFunding={cachedData.ihpFunding}
+                        paFunding={cachedData.paFunding}
+                        sbaFunding={cachedData.sbaFunding}
                         hmgpFunding={cachedData.hmgpFunding}
                         attributionData={cachedData.attributionData}
                         baseUrl={cachedData.baseUrl}
@@ -208,6 +287,11 @@ export default {
         {
             name: 'ealViewId',
             default: 837,
+            hidden: true
+        },
+        {
+            name: 'type',
+            default: 'loss',
             hidden: true
         }
     ],
