9e367745994948ffa6d840b0f487c1ae4d23defd
Eric Conklin <eric.s.conklin@gmail.com>
2024-07-08 10:29:57 -0400

    updates to DMSGraphComp

--- DMSGraphComponent/components/utils.js
+++ DMSGraphComponent/components/utils.js
@@ -5,6 +5,36 @@ import { range as d3range } from "d3-array"
 
 import { useFalcor } from "@availabs/avl-falcor"
 
+const ColorRanges = {}
+
+for (const type in colorbrewer.schemeGroups) {
+  colorbrewer.schemeGroups[type].forEach(name => {
+    const group = colorbrewer[name];
+    for (const length in group) {
+      if (!(length in ColorRanges)) {
+        ColorRanges[length] = [];
+      }
+      ColorRanges[length].push({
+        type: `${ type[0].toUpperCase() }${ type.slice(1) }`,
+        name,
+        category: "Colorbrewer",
+        colors: group[length]
+      })
+    }
+  })
+}
+
+export { ColorRanges };
+
+export const getColorRange = (size, name, reverse=false) => {
+  let range = get(ColorRanges, [size], [])
+    .reduce((a, c) => c.name === name ? c.colors : a, []).slice();
+  if(reverse) {
+    range.reverse()
+  }
+  return range
+}
+
 const NaNValues = ["", null]
 
 export const strictNaN = v => {
